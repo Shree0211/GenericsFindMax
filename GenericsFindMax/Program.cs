1015b29413b3f659499df1b397d229da60c2392b
shree0211 <shreevyas0211@gmail.com>
2022-03-13 16:09:50 +0530

    Finding maximum among 3 numbers

--- GenericsFindMax/Program.cs
+++ GenericsFindMax/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using GenericsFindMax;
+using MaximumGenerics;
+
+Console.WriteLine("-----Find Maximum using Generics------");
+
+// Testing max value in 3 different positions
+Console.WriteLine("Test case 1: " + TestCases.GetString(TestCases.IntTestCase1));
+Maximum.FindIntMax(TestCases.IntTestCase1[0], TestCases.IntTestCase1[1], TestCases.IntTestCase1[2]);
+Console.WriteLine("Test case 2: " + TestCases.GetString(TestCases.IntTestCase2));
+Maximum.FindIntMax(TestCases.IntTestCase2[0], TestCases.IntTestCase2[1], TestCases.IntTestCase2[2]);
+Console.WriteLine("Test case 3: " + TestCases.GetString(TestCases.IntTestCase3));
+Maximum.FindIntMax(TestCases.IntTestCase3[0], TestCases.IntTestCase3[1], TestCases.IntTestCase3[2]);
---
+++ GenericsFindMax/TestCases.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsFindMax
+{
+    internal class TestCases
+    {
+        private static readonly int[] intTestCase1;
+        private static readonly int[] intTestCase2;
+        private static readonly int[] intTestCase3;
+
+        static TestCases()
+        {
+            intTestCase1 = new int[] { 18, 6, 17 };
+            intTestCase2 = new int[] { 1, 9, 6 };
+            intTestCase3 = new int[] { 19, 8, 22 };
+        }
+
+        
+        public static int[] IntTestCase1 => intTestCase1;
+        public static int[] IntTestCase2 => intTestCase2;
+        public static int[] IntTestCase3 => intTestCase3;
+
+        public static string GetString<T>(T[] testCase)
+        {
+            return $"[{testCase[0]}, {testCase[1]}, {testCase[2]}]";
+        }
+    }
+}
