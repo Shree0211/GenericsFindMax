68189ad06b5ef8cfa23e7d44d30349ac22b9ac91
shree0211 <shreevyas0211@gmail.com>
2022-03-13 17:29:17 +0530

    Finding maximum from different parameters

--- GenericsFindMax/Program.cs
+++ GenericsFindMax/Program.cs
@@ -1,29 +1,16 @@
-﻿
-using GenericsFindMax;
-using MaximumGenerics;
+﻿using MaximumGenerics;
 
-Console.WriteLine("-----Find Maximum using Generics------");
+Console.Title = "Find Max using Generics";
+Console.WriteLine("----------Find Max using Generics----------");
 
-// Testing max value in 3 different positions
-Console.WriteLine("Test case 1: " + TestCases.GetString(TestCases.IntTestCase1));
-Maximum.FindIntMax(TestCases.IntTestCase1[0], TestCases.IntTestCase1[1], TestCases.IntTestCase1[2]);
-Console.WriteLine("Test case 2: " + TestCases.GetString(TestCases.IntTestCase2));
-Maximum.FindIntMax(TestCases.IntTestCase2[0], TestCases.IntTestCase2[1], TestCases.IntTestCase2[2]);
-Console.WriteLine("Test case 3: " + TestCases.GetString(TestCases.IntTestCase3));
-Maximum.FindIntMax(TestCases.IntTestCase3[0], TestCases.IntTestCase3[1], TestCases.IntTestCase3[2]);
+// Test of max of int[]
+Console.WriteLine("Integer Tests");
+TestCases.IntTest();
 
+// Test of max of float[]
+Console.WriteLine("Float tests");
+TestCases.FloatTest();
 
-Console.WriteLine("Test case 1: " + TestCases.GetString(TestCases.FloatTestCase1));
-Maximum.FloatMax(TestCases.FloatTestCase1[0], TestCases.FloatTestCase1[1], TestCases.FloatTestCase1[2]);
-Console.WriteLine("Test case 2: " + TestCases.GetString(TestCases.FloatTestCase2));
-Maximum.FloatMax(TestCases.FloatTestCase2[0], TestCases.FloatTestCase2[1], TestCases.FloatTestCase2[2]);
-Console.WriteLine("Test case 3: " + TestCases.GetString(TestCases.FloatTestCase3));
-Maximum.FloatMax(TestCases.FloatTestCase3[0], TestCases.FloatTestCase3[1], TestCases.FloatTestCase3[2]);
-
-Console.WriteLine("Test case 1: " + TestCases.GetString(TestCases.StringTestCase1));
-Maximum.StringMax(TestCases.StringTestCase1[0], TestCases.StringTestCase1[1], TestCases.StringTestCase1[2]);
-Console.WriteLine("Test case 2: " + TestCases.GetString(TestCases.StringTestCase2));
-Maximum.StringMax(TestCases.StringTestCase2[0], TestCases.StringTestCase2[1], TestCases.StringTestCase2[2]);
-Console.WriteLine("Test case 3: " + TestCases.GetString(TestCases.StringTestCase3));
-Maximum.StringMax(TestCases.StringTestCase3[0], TestCases.StringTestCase3[1], TestCases.StringTestCase3[2]);
-
+// Test of max of string[]
+Console.WriteLine("String tests");
+TestCases.StringTest();
--- GenericsFindMax/TestCases.cs
+++ GenericsFindMax/TestCases.cs
@@ -1,52 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MaximumGenerics;
 
-namespace GenericsFindMax
+internal static class TestCases
 {
-    internal class TestCases
+    // Test case array attributes
+    private static readonly int[] intTestCase1;
+    private static readonly int[] intTestCase2;
+    private static readonly int[] intTestCase3;
+    private static readonly float[] floatTestCase1;
+    private static readonly float[] floatTestCase2;
+    private static readonly float[] floatTestCase3;
+    private static readonly string[] stringTestCase1;
+    private static readonly string[] stringTestCase2;
+    private static readonly string[] stringTestCase3;
+
+    // Generic class objects
+    private static Maximum<int> maxInt;
+    private static Maximum<float> maxFloat;
+    private static Maximum<string> maxString;
+
+    /// <summary>
+    /// Initializes the <see cref="TestCase"/> class.
+    /// </summary>
+    static TestCases()
     {
-        private static readonly int[] intTestCase1;
-        private static readonly int[] intTestCase2;
-        private static readonly int[] intTestCase3;
-        private static readonly float[] floatTestCase1;
-        private static readonly float[] floatTestCase2;
-        private static readonly float[] floatTestCase3;
-        private static readonly string[] stringTestCase1;
-        private static readonly string[] stringTestCase2;
-        private static readonly string[] stringTestCase3;
+        intTestCase1 = new int[] { 18, 6, 17 };
+        intTestCase2 = new int[] { 1, 9, 6 };
+        intTestCase3 = new int[] { 19, 8, 22 };
+        floatTestCase1 = new float[] { 9.9f, 1.7f, 7.8f };
+        floatTestCase2 = new float[] { 1.1f, 7.7f, 5.2f };
+        floatTestCase3 = new float[] { 2.1f, 5.5f, 7.7f };
+        stringTestCase1 = new string[] { "Abc", "Apple", "Abcd" };
+        stringTestCase2 = new string[] { "B", "Badminton", "B" };
+        stringTestCase3 = new string[] { "C", "C", "Cabbage" };
+        maxInt = new(intTestCase1);
+        maxFloat = new(floatTestCase1);
+        maxString = new(stringTestCase1);
+    }
 
+    /// <summary>
+    /// returns the specified test case as string.
+    /// </summary>
+    /// <returns>Test case values as string</returns>
+    private static string GetString<T>(T[] testCase)
+    {
+        return $"[{testCase[0]}, {testCase[1]}, {testCase[2]}]";
+    }
 
-        static TestCases()
-        {
-            intTestCase1 = new int[] { 18, 6, 17 };
-            intTestCase2 = new int[] { 1, 9, 6 };
-            intTestCase3 = new int[] { 19, 8, 22 };
-            floatTestCase1 = new float[] { 9.9f, 1.7f, 7.8f };
-            floatTestCase2 = new float[] { 1.1f, 7.7f, 5.2f };
-            floatTestCase3 = new float[] { 2.1f, 5.5f, 7.7f };
-            stringTestCase1 = new string[] { "Abc", "Apple", "Abcd" };
-            stringTestCase2 = new string[] { "B", "Badminton", "B" };
-            stringTestCase3 = new string[] { "C", "C", "Cabbage" };
+    /// <summary>
+    /// Tests the max function for 3 int.
+    /// </summary>
+    public static void IntTest()
+    {
+        Console.WriteLine("Test case 1: " + GetString(intTestCase1));
+        maxInt.Max(intTestCase1[0], intTestCase1[1], intTestCase1[2]);
+        Console.WriteLine("Test case 2: " + GetString(intTestCase2));
+        maxInt.Max(intTestCase2[0], intTestCase2[1], intTestCase2[2]);
+        Console.WriteLine("Test case 3: " + GetString(intTestCase3));
+        maxInt.Max(intTestCase3[0], intTestCase3[1], intTestCase3[2]);
+    }
 
-        }
+    /// <summary>
+    /// Tests the max function for 3 float.
+    /// </summary>
+    public static void FloatTest()
+    {
+        Console.WriteLine("Test case 1: " + GetString(floatTestCase1));
+        maxFloat.Max(floatTestCase1[0], floatTestCase1[1], floatTestCase1[2]);
+        Console.WriteLine("Test case 2: " + GetString(floatTestCase2));
+        maxFloat.Max(floatTestCase2[0], floatTestCase2[1], floatTestCase2[2]);
+        Console.WriteLine("Test case 3: " + GetString(floatTestCase3));
+        maxFloat.Max(floatTestCase3[0], floatTestCase3[1], floatTestCase3[2]);
+    }
 
-
-        public static int[] IntTestCase1 => intTestCase1;
-        public static int[] IntTestCase2 => intTestCase2;
-        public static int[] IntTestCase3 => intTestCase3;
-        public static float[] FloatTestCase1 => floatTestCase1;
-        public static float[] FloatTestCase2 => floatTestCase2;
-        public static float[] FloatTestCase3 => floatTestCase3;
-        public static string[] StringTestCase1 => stringTestCase1;
-        public static string[] StringTestCase2 => stringTestCase2;
-        public static string[] StringTestCase3 => stringTestCase3;
-
-        public static string GetString<T>(T[] testCase)
-        {
-            return $"[{testCase[0]}, {testCase[1]}, {testCase[2]}]";
-        }
+    /// <summary>
+    /// Tests the max function for 3 strings
+    /// </summary>
+    public static void StringTest()
+    {
+        Console.WriteLine("Test case 1: " + GetString(stringTestCase1));
+        maxString.Max(stringTestCase1[0], stringTestCase1[1], stringTestCase1[2]);
+        Console.WriteLine("Test case 2: " + GetString(stringTestCase2));
+        maxString.Max(stringTestCase2[0], stringTestCase2[1], stringTestCase2[2]);
+        Console.WriteLine("Test case 3: " + GetString(stringTestCase3));
+        maxString.Max(stringTestCase3[0], stringTestCase3[1], stringTestCase3[2]);
     }
 }
